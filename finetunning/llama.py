47e4cabe986e065f10b0db34ab8c6b0d73d739e0
MasonN808 <mason.nakamura1@marist.edu>
2024-03-27 09:44:50 -0700

    edit finetunning code

--- finetunning/llama.py
+++ finetunning/llama.py
@@ -1,94 +1,76 @@
-# Motivated by https://www.run.ai/guides/generative-ai/llama-2-fine-tuning
+from datasets import Dataset
 
-import os
-import torch
-from datasets import load_dataset
-from transformers import AutoModelForCausalLM, AutoTokenizer, BitsAndBytesConfig, TrainingArguments, pipeline, logging
-# Assuming LoraConfig and SFTTrainer are available in your environment
-from peft import LoraConfig
+from transformers import TrainingArguments, LlamaTokenizer, LlamaForCausalLM
 from trl import SFTTrainer
-import gc
+from peft import LoraConfig
 
-tokenizer = AutoTokenizer.from_pretrained("meta-llama/Llama-2-7b-hf", token="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")
-model = AutoModelForCausalLM.from_pretrained("meta-llama/Llama-2-7b-hf", token="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")
-model.config.use_cache = True
+MODEL_ID = "openlm-research/open_llama_3b_v2"
 
-# PyTorch memory management
-os.environ["PYTORCH_CUDA_ALLOC_CONF"] = "max_split_size_mb:1024"
-gc.collect()
-torch.cuda.empty_cache()
-
-def display_cuda_memory():
-    print("\n--------------------------------------------------\n")
-    print("torch.cuda.memory_allocated: %fGB" % (torch.cuda.memory_allocated(0)/1024/1024/1024))
-    print("torch.cuda.memory_reserved: %fGB" % (torch.cuda.memory_reserved(0)/1024/1024/1024))
-    print("torch.cuda.max_memory_reserved: %fGB" % (torch.cuda.max_memory_reserved(0)/1024/1024/1024))
-    print("\n--------------------------------------------------\n")
-
-# Define model, dataset, and new model name
-base_model = "meta-llama/Llama-2-7b-hf"
-blocksworld_dataset = "chiayewken/blocksworld"
-new_model = "llama-2-7b-finetunned"
-
-# Load dataset
-dataset = load_dataset(blocksworld_dataset, split="train")
-print(dataset[:5])
-exit()
-# 4-bit Quantization Configuration
-compute_dtype = getattr(torch, "float16")
-quant_config = BitsAndBytesConfig(load_in_4bit=True, bnb_4bit_quant_type="nf4", bnb_4bit_compute_dtype=compute_dtype, bnb_4bit_use_double_quant=False)
-
-# Load model with 4-bit precision
-model = AutoModelForCausalLM.from_pretrained(base_model, quantization_config=quant_config, device_map={"": 0})
-model.config.use_cache = False
-model.config.pretraining_tp = 1
-
-# Load tokenizer
-tokenizer = AutoTokenizer.from_pretrained(base_model, trust_remote_code=True)
+tokenizer = LlamaTokenizer.from_pretrained(MODEL_ID)
+tokenizer.add_eos_token = True
 tokenizer.pad_token = tokenizer.eos_token
-tokenizer.padding_side = "right"
 
-# Set PEFT Parameters
-peft_params = LoraConfig(lora_alpha=16, lora_dropout=0.1, r=64, bias="none", task_type="CAUSAL_LM")
+model = LlamaForCausalLM.from_pretrained(MODEL_ID)
 
-# Define training parameters
-training_params = TrainingArguments(
-    output_dir="./results",
-    num_train_epochs=1,
-    per_device_train_batch_size=4,
-    gradient_accumulation_steps=1,
-    optim="adamw",
-    save_steps=25,
-    logging_steps=25,
-    learning_rate=2e-4,
-    weight_decay=0.001,
-    max_grad_norm=0.3,
-    warmup_ratio=0.03,
-    report_to="tensorboard"
+# Finetune on 1000 examples
+dataset_path = 'finetune_dataset/combined_dataset'
+loaded_dataset = Dataset.load_from_disk(dataset_path)
+
+# Tokenize the dataset
+def tokenize_function(examples):
+    return tokenizer(examples['query'], padding="max_length", truncation=True, max_length=500)
+
+tokenized_dataset = loaded_dataset.map(tokenize_function, batched=True)
+
+tokenized_dataset = tokenized_dataset.map(lambda examples: {'labels': examples['input_ids']}, batched=True)
+# Split the dataset into training and test sets
+tokenized_dataset = tokenized_dataset.train_test_split(test_size=0.1)
+
+# Define the training arguments
+training_args = TrainingArguments(
+    output_dir="./finetunned_models/llama/results",          # output directory for saving model checkpoints
+    num_train_epochs=3,              # number of training epochs
+    per_device_train_batch_size=8,   # batch size for training
+    per_device_eval_batch_size=8,    # batch size for evaluation
+    warmup_steps=100,                # number of warmup steps for learning rate scheduler
+    logging_dir="./logs",            # directory for storing logs
+    logging_steps=10,
+    learning_rate=2e-3,
+    evaluation_strategy="epoch",     # evaluate each `logging_steps`
+    save_strategy='epoch',           # save the model every epoch
+    load_best_model_at_end=True,     # load the best model when finished training
 )
 
-# Initialize the trainer
+lora_config = LoraConfig(
+    r=8,
+    lora_alpha=16,
+    target_modules=[
+        "q_proj",
+        "k_proj",
+        "v_proj",
+        "o_proj",
+    ],
+    task_type="CAUSAL_LM",
+    bias="none",
+)
+
 trainer = SFTTrainer(
     model=model,
-    train_dataset=dataset,
-    peft_config=peft_params,
-    dataset_text_field="text",
-    max_seq_length=None,
     tokenizer=tokenizer,
-    args=training_params,
-    packing=False
+    args=training_args,
+    peft_config=lora_config,
+    train_dataset=tokenized_dataset["train"],
+    eval_dataset=tokenized_dataset["test"],
+    dataset_text_field="query",
 )
 
-# Train the model
+# Fine-tune the model
 trainer.train()
 
-# Save the model and tokenizer
-trainer.model.save_pretrained(new_model)
-trainer.tokenizer.save_pretrained(new_model)
+save_directory = "finetunned_models/llama"
 
-# Test the model
-logging.set_verbosity(logging.CRITICAL)
-prompt = "Who is Leonardo Da Vinci?"
-pipe = pipeline(task="text-generation", model=new_model, tokenizer=new_model, max_length=200)
-result = pipe(f"<s>[INST] {prompt} [/INST]")
-print(result[0]['generated_text'])
+# Save the model
+model.save_pretrained(save_directory)
+
+# Save the tokenizer
+tokenizer.save_pretrained(save_directory)
--- finetunning/mamba.py
+++ finetunning/mamba.py
@@ -1,7 +1,8 @@
 from datasets import load_dataset, Dataset
 
-from transformers import AutoTokenizer, MambaForCausalLM, TrainingArguments, Trainer
-
+from transformers import AutoTokenizer, MambaForCausalLM, TrainingArguments
+from trl import SFTTrainer
+from peft import LoraConfig
 
 MODEL_ID = "state-spaces/mamba-2.8b-hf"
 
@@ -12,53 +13,48 @@ model = MambaForCausalLM.from_pretrained(MODEL_ID)
 dataset_path = 'finetune_dataset/combined_dataset'
 loaded_dataset = Dataset.load_from_disk(dataset_path)
 
-# Split the dataset into training and test sets
-split_dataset = loaded_dataset.train_test_split(test_size=0.2)
-# Access the split parts
-train_dataset = split_dataset['train']
-test_dataset = split_dataset['test']
-
-# Tokenize the dataset
+# Tokenize the datasete
 def tokenize_function(examples):
-    return tokenizer(examples['query'], padding="max_length", truncation=True)
+    # return tokenizer(examples['query'], padding="max_length", truncation=True, max_length=500)
+    return tokenizer(examples['query'], truncation=True)
 
-tokenized_dataset = split_dataset.map(tokenize_function, batched=True)
+tokenized_dataset = loaded_dataset.map(tokenize_function, batched=True)
 
-# Depending on your specific task, you might need to adjust the dataset format.
-# For instance, creating labels for language modeling by shifting the inputs.
 tokenized_dataset = tokenized_dataset.map(lambda examples: {'labels': examples['input_ids']}, batched=True)
-
-# Freeze all parameters first
-for param in model.parameters():
-    param.requires_grad = False
-
-# Unfreeze the parameters of the last few layers
-for layer in model.backbone.layers[-2:]:  # Unfreezing the last 2 layers
-    for param in layer.parameters():
-        param.requires_grad = True
+# Split the dataset into training and test sets
+tokenized_dataset = tokenized_dataset.train_test_split(test_size=0.1)
 
 # Define the training arguments
 training_args = TrainingArguments(
-    output_dir="./results",          # output directory for saving model checkpoints
-    num_train_epochs=3,              # number of training epochs
+    output_dir="./finetunned_models/mamba/results",          # output directory for saving model checkpoints
+    num_train_epochs=2,              # number of training epochs
     per_device_train_batch_size=8,   # batch size for training
     per_device_eval_batch_size=64,    # batch size for evaluation
-    warmup_steps=500,                # number of warmup steps for learning rate scheduler
+    warmup_steps=50,                # number of warmup steps for learning rate scheduler
     weight_decay=0.01,               # strength of weight decay regularization
     logging_dir="./logs",            # directory for storing logs
     logging_steps=10,
+    learning_rate=2e-3,
     evaluation_strategy="epoch",     # evaluate each `logging_steps`
     save_strategy='epoch',           # save the model every epoch
     load_best_model_at_end=True,     # load the best model when finished training
-    metric_for_best_model='accuracy' # use accuracy to evaluate the best model
 )
 
-# Initialize the Trainer
-trainer = Trainer(
+lora_config =  LoraConfig(
+        r=8,
+        target_modules=["x_proj", "embeddings", "in_proj", "out_proj"],
+        task_type="CAUSAL_LM",
+        bias="none"
+)
+
+trainer = SFTTrainer(
     model=model,
+    tokenizer=tokenizer,
     args=training_args,
+    peft_config=lora_config,
     train_dataset=tokenized_dataset["train"],
     eval_dataset=tokenized_dataset["test"],
+    dataset_text_field="query"
 )
 
 # Fine-tune the model
